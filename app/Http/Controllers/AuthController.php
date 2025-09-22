548681df0d10bf30453801d8269def0a3e7a2d0b
Leonel Lopez <technoquo@gmail.com>
2025-09-22 11:04:09 +0200

    fixed redirect home after login public

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,7 +61,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
+            return redirect()->route('home'); // o tu ruta pública
         }
 
         return back()->withErrors([
